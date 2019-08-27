c3d0be9f7fc7f0a5bbd4d7a99ebe3a6322137e2e
luck <496355474@qq.com>
2019-08-28 00:04:33 +0800

    添加德鲁伊连接池

--- goods/src/main/java/org/goods/App.java
+++ goods/src/main/java/org/goods/App.java
@@ -1,13 +1,30 @@
 package org.goods;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+
 /**
- * Hello world!
+ * The type Seata springboot app.
  *
+ * @Description
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+@EnableDubboConfiguration
+@EnableTransactionManagement
+public class App {
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        LOGGER.info("springboot project with seata starting...");
+        SpringApplication.run(App.class, args);
     }
 }
