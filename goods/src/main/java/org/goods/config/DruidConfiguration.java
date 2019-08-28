5c909a54693a8e1cab037c297e12f7e8974e8504
chenxiuyuan <496355474@qq.com>
2019-08-28 17:04:57 +0800

    添加mybits

--- goods/src/main/java/org/goods/config/DruidConfiguration.java
+++ goods/src/main/java/org/goods/config/DruidConfiguration.java
@@ -23,13 +23,17 @@ import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 
 import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.mybatis.spring.SqlSessionFactoryBean;
 
 /**
  * The type Druid configuration.
@@ -43,31 +47,64 @@ public class DruidConfiguration {
 	@Value("${spring.datasource.druid.password}")
 	private String druidPassword;
 
-	/**
-	 * Druid data source druid data source.
-	 *
-	 * @return the druid data source
-	 */
-	@Bean(destroyMethod = "close", initMethod = "init")
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DruidDataSource druidDataSource() {
-		DruidDataSource druidDataSource = new DruidDataSource();
-		return druidDataSource;
-	}
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
 
-	/**
-	 * Data source data source.
-	 *
-	 * @param druidDataSource the druid data source
-	 * @return the data source
-	 */
-	@ConfigurationProperties(prefix = "spring.datasource")
-	@Primary
-	@Bean("dataSource")
-	public DataSource dataSource(DruidDataSource druidDataSource) {
-		DataSourceProxy dataSourceProxy = new DataSourceProxy(druidDataSource);
-		return dataSourceProxy;
-	}
+
+    /**
+     * init durid datasource
+     *
+     * @Return: druidDataSource  datasource instance
+     */
+    @Bean
+    @Primary
+    public DruidDataSource druidDataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(dataSourceProperties.getUrl());
+        druidDataSource.setUsername(dataSourceProperties.getUsername());
+        druidDataSource.setPassword(dataSourceProperties.getPassword());
+        druidDataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        druidDataSource.setInitialSize(0);
+        druidDataSource.setMaxActive(180);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setMinIdle(0);
+        druidDataSource.setValidationQuery("Select 1 from DUAL");
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setMinEvictableIdleTimeMillis(25200000);
+        druidDataSource.setRemoveAbandoned(true);
+        druidDataSource.setRemoveAbandonedTimeout(1800);
+        druidDataSource.setLogAbandoned(true);
+        return druidDataSource;
+    }
+
+
+    /**
+     * init datasource proxy
+     * @Param: druidDataSource  datasource bean instance
+     * @Return: DataSourceProxy  datasource proxy
+     */
+    @Bean
+    public DataSourceProxy dataSourceProxy(DruidDataSource druidDataSource){
+        return new DataSourceProxy(druidDataSource);
+    }
+
+    /**
+     * init mybatis sqlSessionFactory
+     * @Param: dataSourceProxy  datasource proxy
+     * @Return: DataSourceProxy  datasource proxy
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSourceProxy);
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources("classpath*:/mapper/*.xml"));
+        factoryBean.setTransactionFactory(new JdbcTransactionFactory());
+        return factoryBean.getObject();
+    }
 
 	/**
 	 * 注册一个StatViewServlet
--- user/src/main/java/org/user/App.java
+++ user/src/main/java/org/user/App.java
@@ -1,13 +1,28 @@
 package org.user;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+@EnableDubboConfiguration
+@EnableTransactionManagement
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        LOGGER.info("springboot project with seata starting...");
+        SpringApplication.run(App.class, args);
     }
 }
